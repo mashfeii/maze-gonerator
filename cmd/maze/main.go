8d0c17276d1e8eeb2e661899b819d867c06beb4e
mashfeii-manjaro <evil-corporaty@yandex.ru>
2024-11-05 21:48:34 +0300

    chore: renamed entry point

--- cmd/maze/main.go
+++ cmd/maze/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"flag"
+
+	"github.com/es-debug/backend-academy-2024-go-template/config"
+	"github.com/es-debug/backend-academy-2024-go-template/internal/application"
+)
+
 func main() {
-	// TODO: write your code here
+
+	var width, height int
+
+	var generatorType, solverType string
+
+	var draw bool
+
+	flag.IntVar(&width, "width", 30, "Maze width")
+	flag.IntVar(&height, "height", 20, "Maze height")
+	flag.StringVar(&generatorType, "generatorType", "dfs", "Algorithm for maze generation (dfs/kruskal)")
+	flag.StringVar(&solverType, "solverType", "dfs", "Algorithm to solve the maze (astar/prim)")
+	flag.BoolVar(&draw, "draw", false, "Draw the solution")
+
+	flag.Parse()
+	application.Init(config.NewConfig(width, height, generatorType, solverType, draw))
 }
