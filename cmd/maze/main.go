bdfa7debb3eb7bac9d5980b599b88b64e1829881
mashfeii-manjaro <evil-corporaty@yandex.ru>
2024-11-11 00:27:58 +0300

    feat: add cli argument

--- cmd/maze/main.go
+++ cmd/maze/main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"os"
 
 	"github.com/es-debug/backend-academy-2024-go-template/config"
 	"github.com/es-debug/backend-academy-2024-go-template/internal/application"
+	"github.com/es-debug/backend-academy-2024-go-template/internal/infrastructure/errors"
 )
 
 func main() {
@@ -13,13 +15,17 @@ func main() {
 
 	var generatorType, solverType string
 
-	var draw bool
-
 	flag.IntVar(&width, "width", 30, "Maze width")
 	flag.IntVar(&height, "height", 20, "Maze height")
 	flag.StringVar(&generatorType, "generatorType", "dfs", "Algorithm for maze generation (dfs/kruskal)")
 	flag.StringVar(&solverType, "solverType", "dfs", "Algorithm to solve the maze (astar/prim)")
 
 	flag.Parse()
-	application.Init(config.NewConfig(width, height, os.Args[1], generatorType, solverType, draw))
+
+	if len(flag.Args()) < 1 {
+		fmt.Println(errors.NewErrNoCommand().Error())
+		os.Exit(1)
+	}
+
+	application.Init(config.NewConfig(width, height, flag.Args()[len(flag.Args())-1], generatorType, solverType))
 }
